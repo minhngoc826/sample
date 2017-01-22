b68a845a31bd94c5e07da19c9a3cf2e44f36e4c8
minhngoc826 <ngminhngoc826@gmail.com>
2017-01-23 01:28:11 +0700

    chap 7 with basic signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ApplicationRecord
-	before_save { email.downcase! }
-	# before_save { self.email = email.downcase }
-	validates :name, presence: true, length: { maximum: 30 }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  	validates :email, presence: true, length: { maximum: 50},
-                    format: { with: VALID_EMAIL_REGEX },
-                    # uniqueness: true
-                    uniqueness: { case_sensitive: false }
-    has_secure_password
-    validates :password, length: { minimum: 8, maximum: 20 }, presence: true
+  before_save {email.downcase!}
+  validates :name, presence: true, length: {maximum: 30}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 50},
+  format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+
+  has_secure_password
+  validates :password, length: {minimum: 6, maximum: 20}, presence: true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  root "static_pages#home"
+  get "/help", to: "static_pages#help"
+  get "/about", to: "static_pages#about"
+  get "/contact", to: "static_pages#contact"
 
-  get 'users/new'
-  get '/signup', to: 'users#new'
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
 
-  root 'static_pages#home'
-  # get 'static_pages/home'
-  # get 'static_pages/help'
-  # get 'static_pages/about'
-  # get 'static_pages/contact'
-
-  # root 'application#hello'
-
-  get '/help', to: 'static_pages#help'
-  get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
+  resources :users
 
 end
