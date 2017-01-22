8cc26eac0c896eb5ab3a6639dfb6eb8c0c5cfff4
minhngoc826 <ngminhngoc826@gmail.com>
2017-01-23 01:28:11 +0700

    chap 7 with basic signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def show
+    @user = User.find_by id: params[:id]
+    if @user.nil?
+      render file: "public/404.html", layout: false
+    end
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t "controllers.users.wellcome"
+      log_in @user
+      redirect_to @user
+    else
+      flash[:danger] = t "controllers.users.fail"
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
+  end
+
 end
