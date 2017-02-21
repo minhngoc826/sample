d8c165bc2a12d9540a1de9f7d6be7484a95c0623
minhngoc826 <ngminhngoc826@gmail.com>
2017-02-21 11:56:51 +0700

    Chap9 advance login

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,15 +6,16 @@ class SessionController < ApplicationController
     user = User.find_by email: params[:session][:email]
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
       redirect_to current_user
     else
-      flash[:danger] = t ".invalid"
+      flash.now[:danger] = t ".invalid"
       render :new
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_path
   end
 end
