b62154845cf779ede8d9c4e5067f212cec9e2fbf
minhngoc826 <ngminhngoc826@gmail.com>
2017-03-02 15:34:48 +0700

    chap11 activation account

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -5,9 +5,16 @@ class SessionController < ApplicationController
   def create
     user = User.find_by email: params[:session][:email]
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-      redirect_to user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+        redirect_to user
+      else
+        message = t ".not_active"
+        message += t ".check_email"
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = t ".invalid"
       render :new
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222082257) do
+ActiveRecord::Schema.define(version: 20170302091650) do
 
   create_table "mics", force: :cascade do |t|
     t.string   "content"
@@ -22,11 +22,16 @@ ActiveRecord::Schema.define(version: 20170222082257) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
     t.string   "password_digest"
     t.string   "remember_digest"
     t.boolean  "admin"
+    t.string   "activation_digest"
+    t.boolean  "activated"
+    t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_send_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
